74e9c8c92c2adc536e24c43fe6bd28ced5d7f11b
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-02-24 14:16:04 +0100

    show appVersion in footer

--- server/routes/index.js
+++ server/routes/index.js
@@ -20,8 +20,11 @@ const imageCompressionService = new ImageCompressionService();
 //
 // INDEX ROUTE
 //
+const appVersion = require('../package.json').version;
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', {
+    appVersion: appVersion
+  });
 });
 
 //
