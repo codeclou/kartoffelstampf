726f94c2c2818de4b7242ca4b49d6285c8f88bca
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-02-26 16:30:01 +0100

    switch to fela.js initial structure

--- server/express.app.js
+++ server/express.app.js
@@ -20,6 +20,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
+app.use('/npmjs', express.static(path.join(__dirname, 'node_modules')));
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
